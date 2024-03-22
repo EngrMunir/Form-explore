0274659512478b6198f99451efd41c2ac559585b
Munir <smsirajulmonir@gmail.com>
2024-03-22 21:16:53 +0600

    useRef explored

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
-import SimpleForm from './Components/SimpleForm/SimpleForm'
+import RefForm from './Components/RefForm/RefForm'
+// import SavedForm from './Components/SavedForm/SavedForm'
+// import SimpleForm from './Components/SimpleForm/SimpleForm'
 
 
 function App() {
@@ -8,7 +10,9 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <SimpleForm></SimpleForm>  
+      {/* <SimpleForm></SimpleForm>   */}
+      {/* <SavedForm></SavedForm> */}
+      <RefForm></RefForm>
     </>
   )
 }
